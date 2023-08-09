bd5d994df11227731d410e144e681749277883e5
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-08-09 17:11:57 -0300

    Button Types

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,19 @@
-import React from "react";
 import styles from "./Button.module.scss";
 
-function Button(props: { children: string }) {
-  return <button className={styles.botao}>{props.children}</button>;
+function Button(props: {
+  children: string;
+  type?: "button" | "submit" | "reset" | undefined;
+}) {
+  let type = props.type;
+  if (props.type === undefined) {
+    type = "button";
+  }
+
+  return (
+    <button type={type} className={styles.botao}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
